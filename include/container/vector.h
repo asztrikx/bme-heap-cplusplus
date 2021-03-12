542be644563aaff3d19261e28faffd0d5f0b3900
asztrikx <asztrik35@gmail.com>
2021-03-12 23:07:37 +0100

    added ostream operator

--- include/container/vector.h
+++ include/container/vector.h
@@ -1,8 +1,9 @@
 #ifndef VECTOR_H
 #define VECTOR_H
-#include <initializer_list>
 #include "container.h"
+#include <initializer_list>
 #include <cassert>
+#include <iostream>
 
 namespace Container {
 
@@ -195,6 +196,21 @@ class Vector {
 template <typename T>
 Vector<T> operator+(T const &value, Vector<T> const &vector) {
 	return vector + value;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &stream, Vector<T> const &vector) {
+	stream << "[" << vector.length << "]";
+	stream << "(";
+	for (int i = 0; i < vector.length; i++) {
+		stream << vector[i];
+		if (i < vector.length - 1) {
+			stream << ", ";
+		}
+	}
+	stream << ")";
+
+	return stream;
 }
 
 } // namespace Container
