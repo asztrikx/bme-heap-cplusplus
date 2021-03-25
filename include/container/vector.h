b66ccaefc1ae1fe64023321c33b02a8e28794038
asztrikx <asztrik35@gmail.com>
2021-03-25 11:31:59 +0100

    moved template opretors back as they dont work

--- include/container/vector.h
+++ include/container/vector.h
@@ -206,10 +206,24 @@ class Vector {
 };
 
 template <typename T>
-Vector<T> operator+(T const &value, Vector<T> const &vector);
+Vector<T> operator+(T const &value, Vector<T> const &vector) {
+	return vector + value;
+}
 
 template <typename T>
-std::ostream &operator<<(std::ostream &stream, Vector<T> const &vector);
+std::ostream &operator<<(std::ostream &stream, Vector<T> const &vector) {
+	stream << "[" << vector.length() << "]";
+	stream << "(";
+	for (int i = 0; i < vector.length(); i++) {
+		stream << vector[i];
+		if (i < vector.length() - 1) {
+			stream << ", ";
+		}
+	}
+	stream << ")";
+
+	return stream;
+}
 
 } // namespace Container
 
--- include/heap/binaryHeap.h
+++ include/heap/binaryHeap.h
@@ -265,9 +265,13 @@ class BinaryHeap {
 };
 
 template <typename T>
-BinaryHeap<T> operator+(T const &value, BinaryHeap<T> const &binaryHeap);
+BinaryHeap<T> operator+(T const &value, BinaryHeap<T> const &binaryHeap) {
+	return binaryHeap + value;
+}
 template <typename T>
-BinaryHeap<T> operator+(Container::Vector<T> const &values, BinaryHeap<T> const &binaryHeap);
+BinaryHeap<T> operator+(Container::Vector<T> const &values, BinaryHeap<T> const &binaryHeap) {
+	return binaryHeap + values;
+}
 
 } // namespace Heap
 
